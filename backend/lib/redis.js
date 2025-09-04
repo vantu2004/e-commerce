191b45dc5b8bd1201a5a67f1b95fd603ff22e434
Thang Huynh <thanghuynh0706@gmail.com>
2025-09-05 00:04:00 +0700

    feat(Profile): updateUser

--- backend/lib/redis.js
+++
@@ -1,8 +0,0 @@
-import Redis from "ioredis";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const redis = new Redis(process.env.UPSTASH_REDIS_URL);
-
-export default redis;
--- backend/lib/stripe.js
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-export default stripe;
