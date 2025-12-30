4937bb686433704b189cc0b62d546a054927098b
vantu <tulevan526@gmail.com>
2025-12-30 15:51:20 +0700

    fix(backend): correct frontend dist path in production

--- backend/server.js
+++ backend/server.js
@@ -38,10 +38,11 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticRoutes);
 
+console.log(__dirname);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
+  app.get(/^(?!\/api\/).*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
