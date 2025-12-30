a6f5fdee8fcac809a2757f67c61e63693ae4de0e
vantu <tulevan526@gmail.com>
2025-12-30 15:32:13 +0700

    fix(backend): correct frontend dist path in production

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use("/api/analytics", analyticRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
   app.get(/^(?!\/api\/).*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
 
